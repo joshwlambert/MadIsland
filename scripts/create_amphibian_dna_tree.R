7e6ac9e007b7d2c83c107e22be2066ef30ace857
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-06-14 21:14:07 +0200

    added create_amphibian_dna_tree and create_squamate_dna_tree script

---
+++ scripts/create_amphibian_dna_tree.R
@@ -0,0 +1,49 @@
+library(MadIsland)
+
+# load the complete trees
+amphibian_posterior_complete <- ape::read.nexus(
+  file = system.file(
+    "extdata/Jetz_Pyron_complete_posterior_100.nex",
+    package = "MadIsland"
+  )
+)
+
+# load species names that with whether they have DNA data
+amphibian_dna_phylogeny <- read_checklist(
+  file_name = "amphibian_dna_phylogeny.csv"
+)
+
+# check that the data has loaded correctly and that it has the correct data
+head(amphibian_dna_phylogeny)
+all(
+  colnames(amphibian_dna_phylogeny) ==
+    c("Name", "Data", "Genes")
+)
+
+# change species names to underscore separated
+amphibian_dna_phylogeny$Name <- gsub(
+  pattern = " ",
+  replacement = "_",
+  x = amphibian_dna_phylogeny$Name
+)
+
+# check that Data and Genes columns correspond
+dna_data <- amphibian_dna_phylogeny$Data > 0
+num_genes <- amphibian_dna_phylogeny$Genes > 0
+identical(dna_data, num_genes)
+
+# prune trees to only include genetic species
+dna_species <- amphibian_dna_phylogeny$Name[which(dna_data)]
+Jetz_Pyron_dna_posterior_100 <- lapply(
+  amphibian_posterior_complete,
+  ape::keep.tip,
+  tip = dna_species
+)
+class(Jetz_Pyron_dna_posterior_100) <- "multiPhylo"
+
+# save DNA-only trees
+# 1) the multi_island_tbl
+ape::write.nexus(
+  Jetz_Pyron_dna_posterior_100,
+  file = "inst/extdata/Jetz_Pyron_dna_posterior_100.nex"
+)
---
+++ scripts/create_squamate_dna_tree.R
@@ -0,0 +1,49 @@
+library(MadIsland)
+
+# load the complete trees
+squamate_posterior_complete <- ape::read.nexus(
+  file = system.file(
+    "extdata/Tonini_complete_posterior_100.nex",
+    package = "MadIsland"
+  )
+)
+
+# load species names that with whether they have DNA data
+squamate_dna_phylogeny <- read_checklist(
+  file_name = "squamate_dna_phylogeny.csv"
+)
+
+# check that the data has loaded correctly and that it has the correct data
+head(squamate_dna_phylogeny)
+all(
+  colnames(squamate_dna_phylogeny) ==
+    c("Name", "Genes", "Length")
+)
+
+# change species names to underscore separated
+squamate_dna_phylogeny$Name <- gsub(
+  pattern = " ",
+  replacement = "_",
+  x = squamate_dna_phylogeny$Name
+)
+
+# check that Data and Genes columns correspond
+dna_data <- squamate_dna_phylogeny$Genes > 0
+length_genes <- squamate_dna_phylogeny$Genes > 0
+identical(dna_data, length_genes)
+
+# prune trees to only include genetic species
+dna_species <- squamate_dna_phylogeny$Name[which(dna_data)]
+Tonini_dna_posterior_100 <- lapply(
+  squamate_posterior_complete,
+  ape::keep.tip,
+  tip = dna_species
+)
+
+class(Tonini_dna_posterior_100) <- "multiPhylo"
+
+# save DNA-only trees
+ape::write.nexus(
+  Tonini_dna_posterior_100,
+  file = "inst/extdata/Tonini_dna_posterior_100.nex"
+)
