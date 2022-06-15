4a923f9dbaf5a118f6944e74b1bc1c6aec357d03
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-06-15 18:54:56 +0200

    updated extract_amphibian_dna and extract_amphibian_complete scripts

--- scripts/extract_amphibians_complete.R
+++ scripts/extract_amphibians_complete.R
@@ -1,9 +1,14 @@
+library(MadIsland)
+
 # load madagascar amphibians species data table
 data("madagascar_amphibians_complete", package = "MadIsland")
 
 # check that the data has loaded correctly and that it has the correct data
-head(madagascar_amphibians)
-all(colnames(madagascar_amphibians) == c("tip_labels", "tip_endemicity_status"))
+head(madagascar_amphibians_complete)
+all(
+  colnames(madagascar_amphibians_complete) ==
+    c("tip_labels", "tip_endemicity_status")
+)
 
 # load the full raw data checklist of amphibian species of madagascar
 amphibian_checklist <- read_checklist(file_name = "amphibian_checklist.csv")
@@ -12,13 +17,14 @@ amphibian_checklist <- read_checklist(file_name = "amphibian_checklist.csv")
 head(amphibian_checklist)
 all(colnames(amphibian_checklist) == c(
   "Genus", "Species", "Subspecies", "Family", "Order", "Common_Name",
-  "Our_Name", "Name_In_Tree", "Sampled", "DNA_In_Tree", "Extinct_Extant",
-  "Status_Species", "DAISIE_Species_Status", "Remove_Species"
+  "Name_In_Tree", "Sampled", "DNA_In_Tree", "Extinct_Extant",
+  "Status_Species", "DAISIE_Status_Species", "Remove_Species"
 ))
 
 # count the number of missing species for each genus
 missing_amphibian_species <- count_missing_species(
-  checklist = amphibian_checklist
+  checklist = amphibian_checklist,
+  dna_or_complete = "complete"
 )
 
 # look at missing species to check
@@ -46,18 +52,15 @@ gc()
 endemicity_status_complete <- lapply(
   complete_phylos,
   DAISIEprep::create_endemicity_status,
-  island_species = madagascar_amphibians
+  island_species = madagascar_amphibians_complete
 )
 
 # combine tree and endemicity status
-complete_multi_phylods <- list()
-for (i in seq_along(complete_phylos)) {
-  message("Converting phylo ", i, " of ", length(complete_phylos))
-  complete_multi_phylods[[i]] <- phylobase::phylo4d(
-    complete_phylos[[i]],
-    endemicity_status_complete[[i]]
-  )
-}
+complete_multi_phylods <- mapply(
+  phylobase::phylo4d,
+  complete_phylos,
+  endemicity_status_complete
+)
 
 # extract island community using min algorithm
 multi_island_tbl_complete <- DAISIEprep::multi_extract_island_species(
@@ -66,24 +69,43 @@ multi_island_tbl_complete <- DAISIEprep::multi_extract_island_species(
   verbose = TRUE
 )
 
-missing_genus <- lapply(multi_island_tbl_complete, unique_missing_species)
+# determine which island clade the missing species should be assigned to
+missing_genus_complete <- lapply(
+  multi_island_tbl_complete,
+  unique_missing_species
+)
 
 # add missing species that match genera found in the island tbl
 multi_island_tbl_complete <- mapply(
   add_phylo_missing_species,
-  missing_genus,
+  missing_genus_complete,
   multi_island_tbl_complete,
   missing_species = list(missing_amphibian_species)
 )
 
 # remove missing species that have already been inserted into the island tbl
-no_phylo_missing_amphibian_species <- lapply(
-  missing_genus,
+no_phylo_missing_amphibian_species_complete <- lapply(
+  missing_genus_complete,
   rm_phylo_missing_species,
   missing_species = missing_amphibian_species
 )
 
-mini_stem_age <- DAISIEprep::extract_stem_age(
+# check that all the missing species that have not already been assigned are the
+# same between different trees in the posterior
+sapply(
+  list(
+    no_phylo_missing_amphibian_species_complete[[2]]$clade_name,
+    no_phylo_missing_amphibian_species_complete[[3]]$clade_name
+  ), identical,
+  no_phylo_missing_amphibian_species_complete[[1]]$clade_name
+)
+
+# check which missing species that are not already assigned have stem ages in
+# the tree, when no stem is found only one tree needs to be checked as each
+# tree in the posterior contains the same species and just differs in branch
+# lengths and topology, when the stem age is found, a stem age from each tree is
+# required
+DAISIEprep::extract_stem_age(
   genus_name = "Mini",
   phylod = complete_multi_phylods[[1]],
   extraction_method = "min"
@@ -98,7 +120,8 @@ multi_island_tbl_complete <- lapply(
   missing_species = 3,
   branching_times = 88,
   min_age = NA,
-  species = c("Mini_ature", "Mini_mum", "Mini_scule"))
+  species = c("Mini_ature", "Mini_mum", "Mini_scule")
+)
 
 # convert to daisie data table
 daisie_datatable_complete <- lapply(
--- scripts/extract_amphibians_dna.R
+++ scripts/extract_amphibians_dna.R
@@ -30,92 +30,128 @@ missing_amphibian_species <- count_missing_species(
 # look at missing species to check
 missing_amphibian_species
 
-# load the complete trees
-amphibian_posterior_complete <- ape::read.nexus(
+# load the DNA-only trees
+amphibian_posterior_dna <- ape::read.nexus(
   file = system.file(
-    "extdata/Jetz_Pyron_complete_posterior_100.nex",
+    "extdata/Jetz_Pyron_dna_posterior_100.nex",
     package = "MadIsland"
   )
 )
 
 #delete
-amphibian_posterior_complete <- amphibian_posterior_complete[1:3]
+amphibian_posterior_dna <- amphibian_posterior_dna[1:3]
 
 # convert trees to phylo4 objects
-complete_phylos <- lapply(amphibian_posterior_complete, phylobase::phylo4)
+dna_phylos <- lapply(amphibian_posterior_dna, phylobase::phylo4)
 
 # remove phylo objects to free up some memory
-rm(amphibian_posterior_complete)
+rm(amphibian_posterior_dna)
 gc()
 
 # create endemicity status data frame
-endemicity_status_complete <- lapply(
-  complete_phylos,
+endemicity_status_dna <- lapply(
+  dna_phylos,
   DAISIEprep::create_endemicity_status,
-  island_species = madagascar_amphibians
+  island_species = madagascar_amphibians_dna
 )
 
 # combine tree and endemicity status
-complete_multi_phylods <- list()
-for (i in seq_along(complete_phylos)) {
-  message("Converting phylo ", i, " of ", length(complete_phylos))
-  complete_multi_phylods[[i]] <- phylobase::phylo4d(
-    complete_phylos[[i]],
-    endemicity_status_complete[[i]]
-  )
-}
+dna_multi_phylods <- mapply(
+  phylobase::phylo4d,
+  dna_phylos,
+  endemicity_status_dna
+)
 
 # extract island community using min algorithm
-multi_island_tbl_complete <- DAISIEprep::multi_extract_island_species(
-  multi_phylod = complete_multi_phylods,
+multi_island_tbl_dna <- DAISIEprep::multi_extract_island_species(
+  multi_phylod = dna_multi_phylods,
   extraction_method = "min",
   verbose = TRUE
 )
 
-missing_genus <- lapply(multi_island_tbl_complete, unique_missing_species)
+# determine which island clade the missing species should be assigned to
+missing_genus_dna <- lapply(
+  multi_island_tbl_dna,
+  unique_missing_species
+)
 
 # add missing species that match genera found in the island tbl
-multi_island_tbl_complete <- mapply(
+multi_island_tbl_dna <- mapply(
   add_phylo_missing_species,
-  missing_genus,
-  multi_island_tbl_complete,
+  missing_genus_dna,
+  multi_island_tbl_dna,
   missing_species = list(missing_amphibian_species)
 )
 
 # remove missing species that have already been inserted into the island tbl
-no_phylo_missing_amphibian_species <- lapply(
-  missing_genus,
+no_phylo_missing_amphibian_species_dna <- lapply(
+  missing_genus_dna,
   rm_phylo_missing_species,
   missing_species = missing_amphibian_species
 )
 
-mini_stem_age <- DAISIEprep::extract_stem_age(
-  genus_name = "Mini",
-  phylod = complete_multi_phylods[[1]],
+# check that all the missing species that have not already been assigned are the
+# same between different trees in the posterior
+sapply(
+  list(
+    no_phylo_missing_amphibian_species_dna[[2]]$clade_name,
+    no_phylo_missing_amphibian_species_dna[[3]]$clade_name
+  ), identical,
+  no_phylo_missing_amphibian_species_dna[[1]]$clade_name
+)
+
+# check which missing species that are not already assigned have stem ages in
+# the tree, when no stem is found only one tree needs to be checked as each
+# tree in the posterior contains the same species and just differs in branch
+# lengths and topology, when the stem age is found, a stem age from each tree is
+# required
+DAISIEprep::extract_stem_age(
+  genus_name = "Madecassophryne",
+  phylod = dna_multi_phylods[[1]],
   extraction_method = "min"
 )
 
+DAISIEprep::extract_stem_age(
+  genus_name = "Mini",
+  phylod = dna_multi_phylods[[1]],
+  extraction_method = "min"
+)
+
+# add the Madecassophryne as an island age max age as there is no stem age in
+# the tree
+multi_island_tbl_dna <- lapply(
+  multi_island_tbl_dna,
+  DAISIEprep::add_island_colonist,
+  clade_name = "Madecassophryne_truebae",
+  status = "endemic",
+  missing_species = 1,
+  branching_times = 88,
+  min_age = NA,
+  species = c("Madecassophryne_truebae")
+)
+
 # add the Mini as an island age max age as there is no stem age in the tree
-multi_island_tbl_complete <- lapply(
-  multi_island_tbl_complete,
+multi_island_tbl_dna <- lapply(
+  multi_island_tbl_dna,
   DAISIEprep::add_island_colonist,
   clade_name = "Mini_ature",
   status = "endemic",
   missing_species = 3,
   branching_times = 88,
   min_age = NA,
-  species = c("Mini_ature", "Mini_mum", "Mini_scule"))
+  species = c("Mini_ature", "Mini_mum", "Mini_scule")
+)
 
 # convert to daisie data table
-daisie_datatable_complete <- lapply(
-  multi_island_tbl_complete,
+daisie_datatable_dna <- lapply(
+  multi_island_tbl_dna,
   DAISIEprep::as_daisie_datatable,
   island_age = 88
 )
 
 # convert to daisie data list
-daisie_data_list_complete <- lapply(
-  daisie_datatable_complete,
+daisie_data_list_dna <- lapply(
+  daisie_datatable_dna,
   DAISIEprep::create_daisie_data,
   island_age = 88,
   num_mainland_species = 1000
@@ -124,18 +160,18 @@ daisie_data_list_complete <- lapply(
 # save the main data outputs
 # 1) the multi_island_tbl
 saveRDS(
-  object = multi_island_tbl_complete,
-  file = "inst/extdata/amphibian_island_tbl_complete.rds"
+  object = multi_island_tbl_dna,
+  file = "inst/extdata/amphibian_island_tbl_dna.rds"
 )
 
 # 2) the DAISIE data table
 saveRDS(
-  object = daisie_datatable_complete,
-  file = "inst/extdata/amphibian_daisie_datatable_complete.rds"
+  object = daisie_datatable_dna,
+  file = "inst/extdata/amphibian_daisie_datatable_dna.rds"
 )
 
 # 3) the DAISIE data list
 saveRDS(
-  object = daisie_data_list_complete,
-  file = "inst/extdata/amphibian_daisie_data_list_complete.rds"
+  object = daisie_data_list_dna,
+  file = "inst/extdata/amphibian_daisie_data_list_dna.rds"
 )
