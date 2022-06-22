bc89f3a0cee8a165e989cc25e6763287be3b8c0d
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-06-22 23:12:30 +0200

    WIP updating extract_data scripts

--- inst/scripts/extract_data/amphibians/extract_amphibians_complete.R
+++ inst/scripts/extract_data/amphibians/extract_amphibians_complete.R
@@ -1,98 +1,5 @@
 library(MadIsland)
 
-# load madagascar amphibians species data table
-data("madagascar_amphibians_complete", package = "MadIsland")
-
-# check that the data has loaded correctly and that it has the correct data
-head(madagascar_amphibians_complete)
-all(
-  colnames(madagascar_amphibians_complete) ==
-    c("tip_labels", "tip_endemicity_status")
-)
-
-# load the full raw data checklist of amphibian species of madagascar
-amphibian_checklist <- read_checklist(file_name = "amphibian_checklist.csv")
-
-# check that the data has loaded correctly and that it has the correct data
-head(amphibian_checklist)
-all(colnames(amphibian_checklist) == c(
-  "Genus", "Species", "Subspecies", "Family", "Order", "Common_Name",
-  "Name_In_Tree", "Sampled", "DNA_In_Tree", "Extinct_Extant",
-  "Status_Species", "DAISIE_Status_Species", "Remove_Species"
-))
-
-# count the number of missing species for each genus
-missing_amphibian_species <- count_missing_species(
-  checklist = amphibian_checklist,
-  dna_or_complete = "complete"
-)
-
-# look at missing species to check
-missing_amphibian_species
-
-# load the complete trees
-amphibian_posterior_complete <- ape::read.nexus(
-  file = system.file(
-    "extdata", "phylos", "Jetz_Pyron_complete_posterior_100.nex",
-    package = "MadIsland"
-  )
-)
-
-#delete
-amphibian_posterior_complete <- amphibian_posterior_complete[1:3]
-
-# convert trees to phylo4 objects
-complete_phylos <- lapply(amphibian_posterior_complete, phylobase::phylo4)
-
-# remove phylo objects to free up some memory
-rm(amphibian_posterior_complete)
-gc()
-
-# create endemicity status data frame
-endemicity_status_complete <- lapply(
-  complete_phylos,
-  DAISIEprep::create_endemicity_status,
-  island_species = madagascar_amphibians_complete
-)
-
-# combine tree and endemicity status
-complete_multi_phylods <- mapply(
-  phylobase::phylo4d,
-  complete_phylos,
-  endemicity_status_complete
-)
-
-# extract island community using min algorithm
-multi_island_tbl_complete <- DAISIEprep::multi_extract_island_species(
-  multi_phylod = complete_multi_phylods,
-  extraction_method = "min",
-  verbose = TRUE
-)
-
-# determine which island clade the missing species should be assigned to
-missing_genus_complete <- lapply(
-  multi_island_tbl_complete,
-  unique_missing_species
-)
-
-# add missing species that match genera found in the island tbl
-multi_island_tbl_complete <- mapply(
-  add_phylo_missing_species,
-  missing_genus_complete,
-  multi_island_tbl_complete,
-  missing_species = list(missing_amphibian_species)
-)
-
-# remove missing species that have already been inserted into the island tbl
-no_phylo_missing_amphibian_species_complete <- lapply(
-  missing_genus_complete,
-  rm_phylo_missing_species,
-  missing_species = missing_amphibian_species
-)
-
-##########################################################
-##########################################################
-
 island_data <- extract_species(
   checklist_file_name = "amphibian_checklist.csv",
   phylo_file_name = "Jetz_Pyron_complete_posterior_100.nex",
@@ -100,19 +7,6 @@ island_data <- extract_species(
   daisie_status = FALSE,
   extraction_method = "min"
 )
-
-identical(island_data$multi_island_tbl, multi_island_tbl_complete)
-identical(
-  island_data$no_phylo_missing_species,
-  no_phylo_missing_amphibian_species_complete
-)
-waldo::compare(island_data$multi_island_tbl, multi_island_tbl_complete)
-waldo::compare(
-  island_data$no_phylo_missing_species,
-  no_phylo_missing_amphibian_species_complete
-)
-##########################################################
-##########################################################
 
 # check that all the missing species that have not already been assigned are the
 # same between different trees in the posterior
