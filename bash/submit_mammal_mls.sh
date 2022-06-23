674a5e63ae0c4a37a5b018a962f2421efb5d6ed9
joshwlambert <j.w.l.lambert@rug.nl>
2022-06-23 20:07:39 +0200

    change partition to regular

--- bash/submit_mammal_mls.sh
+++ bash/submit_mammal_mls.sh
@@ -5,7 +5,7 @@
 #SBATCH --job-name=mammal_mls
 #SBATCH --output=logs/mammal_mls.log
 #SBATCH --mem=5GB
-#SBATCH --partition=gelifes
+#SBATCH --partition=regular
 
 sbatch --job-name=mammals_mls DAISIEutils/bash/submit_run_daisie_ml.sh mammal_daisie_data_list_complete_ds_asr cr_dd MadIsland 0 NULL lsodes subplex TRUE
 
