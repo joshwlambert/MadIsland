39a9a1b18bac1388a7f0ec3b0ac46614f90639bb
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-06-24 16:59:25 +0200

    updated pilot

--- bash/pilot.sh
+++ bash/pilot.sh
@@ -8,4 +8,4 @@
 #SBATCH --partition=regular
 #SBATCH --array=1-3
 
-sbatch --job-name=mammals_mls DAISIEutils/bash/submit_run_daisie_ml.sh mammal_data_1 cr_di MadIsland 0 NULL lsodes subplex TRUE ${SLURM_ARRAY_TASK_ID}
+sbatch --job-name=mammals_mls DAISIEutils/bash/submit_run_daisie_ml.sh mammal_daisie_data_list_complete_ds_asr cr_di MadIsland 0 NULL lsodes subplex TRUE ${SLURM_ARRAY_TASK_ID}
