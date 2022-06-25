458cd6192b2f2d37342d17ada284c651b45970b9
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-06-25 15:45:17 +0200

    added extract_amphibians_complete_asr bash script

---
+++ inst/bash/extract_data/amphibians/extract_amphibians_complete_asr.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --time=70:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=extract_amphibians_complete_asr
+#SBATCH --output=logs/extract_amphibians_complete_asr.log
+#SBATCH --mem=5GB
+#SBATCH --partition=regular
+
+ml R
+Rscript -e "MadIsland/inst/scripts/extract_data/amphibians/extract_amphibians_complete_asr.R"
