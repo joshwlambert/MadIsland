31aa6ff92a7641117f122ef781e10b6a052c3695
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-06-25 15:30:46 +0200

    added squamate data, data-raw and documentation

---
+++ data-raw/squamate_daisie_data_list_complete_ds_asr.R
@@ -0,0 +1,15 @@
+## code to prepare `squamate_daisie_data_list_complete_ds_asr` dataset goes
+## here
+
+squamate_daisie_data_list_complete_ds_asr <- readRDS(
+  file = system.file(
+    "extdata",
+    "extracted_data",
+    "squamate_data",
+    "squamate_daisie_data_list_complete_ds_asr.rds",
+    package = "MadIsland",
+    mustWork = TRUE
+  )
+)
+
+usethis::use_data(squamate_daisie_data_list_complete_ds_asr, overwrite = TRUE)
