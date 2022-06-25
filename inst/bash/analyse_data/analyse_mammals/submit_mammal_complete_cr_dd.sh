f4d8d4fc83e447d8cfd8597c4495f60e227dca22
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-06-25 10:36:44 +0200

    changed submit_mammal_complete_cr_di to cr_dd

--- inst/bash/analyse_data/analyse_mammals/submit_mammal_complete_cr_dd.sh
+++ inst/bash/analyse_data/analyse_mammals/submit_mammal_complete_cr_dd.sh
@@ -3,14 +3,14 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=mammal_mls
-#SBATCH --output=logs/submit_mammal_mls_cr_di_%a.log
+#SBATCH --output=logs/submit_mammal_mls_cr_dd_%a.log
 #SBATCH --mem=5GB
 #SBATCH --partition=regular
 #SBATCH --array=1-3
 
 sbatch DAISIEutils/bash/submit_run_daisie_ml.sh \
   mammal_daisie_data_list_complete_ds_asr \
-  cr_di \
+  cr_dd \
   MadIsland \
   0 \
   NULL \
