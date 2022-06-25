aef502d9bb659db31dc2b2d0c69ad592d4352b9f
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-06-25 16:11:30 +0200

    fixing extract_amphibians_complete_asr bash script

--- inst/bash/extract_data/amphibians/extract_amphibians_complete_asr.sh
+++ inst/bash/extract_data/amphibians/extract_amphibians_complete_asr.sh
@@ -8,4 +8,4 @@
 #SBATCH --partition=regular
 
 ml R
-Rscript -e 'MadIsland/inst/scripts/extract_data/amphibians/extract_amphibians_complete_asr.R'
+Rscript MadIsland/inst/scripts/extract_data/amphibians/extract_amphibians_complete_asr.R
