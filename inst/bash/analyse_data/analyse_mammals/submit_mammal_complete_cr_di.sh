6a578c015d9eff048512c037ad583d32edb1d107
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-06-25 10:31:20 +0200

    updated submit_mammal_complete_cr_di

--- inst/bash/analyse_data/analyse_mammals/submit_mammal_complete_cr_di.sh
+++ inst/bash/analyse_data/analyse_mammals/submit_mammal_complete_cr_di.sh
@@ -3,17 +3,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=mammal_mls
-#SBATCH --output=/logs/submit_mammal_mls_cr_di_%a.log
+#SBATCH --output=logs/submit_mammal_mls_cr_di_%a.log
 #SBATCH --mem=5GB
 #SBATCH --partition=regular
 #SBATCH --array=1-3
 
-sbatch DAISIEutils/bash/submit_run_daisie_ml.sh mammal_daisie_data_list_complete_ds_asr \
-  cr_di \
-  MadIsland \
-  0 \
-  NULL \
-  lsodes \
-  subplex \
-  TRUE \
-  ${SLURM_ARRAY_TASK_ID}
+sbatch DAISIEutils/bash/submit_run_daisie_ml.sh mammal_daisie_data_list_complete_ds_asr cr_di MadIsland 0 NULL lsodes subplex TRUE ${SLURM_ARRAY_TASK_ID}
