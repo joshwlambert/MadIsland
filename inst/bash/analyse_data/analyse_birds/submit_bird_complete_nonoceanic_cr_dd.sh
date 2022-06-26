1d005f9522c10a95bb6b6cd3c6cbb038c3227580
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-06-26 17:15:29 +0200

    added nonoceanic analysis bash scripts for each group

---
+++ inst/bash/analyse_data/analyse_birds/submit_bird_complete_nonoceanic_cr_dd.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --time=00:10:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=bird_complete_mls
+#SBATCH --output=logs/submit_bird_complete_mls_nonoceanic_cr_dd_%a.log
+#SBATCH --mem=5GB
+#SBATCH --partition=regular
+#SBATCH --array=1-3
+
+sbatch DAISIEutils/bash/submit_run_daisie_ml.sh \
+  bird_daisie_data_list_complete_ds_asr \
+  nonoceanic_cr_dd \
+  MadIsland \
+  0 \
+  NULL \
+  lsodes \
+  subplex \
+  TRUE \
+  ${SLURM_ARRAY_TASK_ID}
