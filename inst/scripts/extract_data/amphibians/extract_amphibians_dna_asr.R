b583b7864d9cd6b5573d0adfa8c4b038527fb2d7
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-06-30 11:08:49 +0200

    updated extract amphibian data scripts

--- inst/scripts/extract_data/amphibians/extract_amphibians_dna_asr.R
+++ inst/scripts/extract_data/amphibians/extract_amphibians_dna_asr.R
@@ -9,49 +9,37 @@ island_data <- MadIsland::extract_species(
 )
 
 multi_island_tbl_dna <- island_data$multi_island_tbl
-no_phylo_missing_species <- island_data$no_phylo_missing_species
+no_island_tbl_missing_species <- island_data$no_island_tbl_missing_species
 dna_multi_phylods <- island_data$phylods
 
-# check which missing species that are not already assigned have stem ages in
-# the tree, when no stem is found only one tree needs to be checked as each
-# tree in the posterior contains the same species and just differs in branch
-# lengths and topology, when the stem age is found, a stem age from each tree is
-# required
-DAISIEprep::extract_stem_age(
-  genus_name = "Madecassophryne",
-  phylod = dna_multi_phylods[[1]],
-  extraction_method = "asr"
-)
+# if the species has no close relatives in the island data then it can be
+# checked whether those missing species that are not already assigned have
+# stem ages in the tree, when no stem is found only one tree needs to be checked
+# as each tree in the posterior contains the same species and just differs in
+# branch lengths and topology, when the stem age is found, a stem age from each
+# tree is required. In cases where a close relative is known from taxonomic
+# data and is already in the island data the stem age does not need to be
+# searched for as the species can be added as a missing species of its close
+# relative on the island
 
-DAISIEprep::extract_stem_age(
-  genus_name = "Mini",
-  phylod = dna_multi_phylods[[1]],
-  extraction_method = "asr"
-)
-
-# add the Madecassophryne as an island age max age as there is no stem age in
-# the tree
+# add the Madecassophryne as a missing species of the clade with
+# Plethodontohyla_brevipes in it as it is a member of the Cophylinae radiation
+# on Madagascar
 multi_island_tbl_dna <- lapply(
   multi_island_tbl_dna,
-  DAISIEprep::add_island_colonist,
-  clade_name = "Madecassophryne_truebae",
-  status = "endemic",
-  missing_species = 0,
-  branching_times = 88,
-  min_age = NA,
-  species = c("Madecassophryne_truebae")
+  add_missing_species,
+  num_missing_species = 1,
+  species_name = "Plethodontohyla_brevipes"
 )
 
-# add the Mini as an island age max age as there is no stem age in the tree
+# add the Mini as a missing species of the clade with
+# Plethodontohyla_brevipes in it as it is a member of the Cophylinae radiation
+# on Madagascar
 multi_island_tbl_dna <- lapply(
   multi_island_tbl_dna,
-  DAISIEprep::add_island_colonist,
-  clade_name = "Mini_ature",
-  status = "endemic",
-  missing_species = 2,
-  branching_times = 88,
-  min_age = NA,
-  species = c("Mini_ature", "Mini_mum", "Mini_scule")
+  add_missing_species,
+  num_missing_species = 3,
+  species_name = "Plethodontohyla_brevipes"
 )
 
 # convert to daisie data table
