3128e584bb8405f0f0a2d3b8e83bbee5b53a8ae7
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-07-09 18:23:15 +0200

    moved plot_stts to individual group folder

---
+++ inst/scripts/plots/amphibians/plot_amphibian_stt.R
@@ -0,0 +1,13 @@
+data("amphibian_daisie_data_list_complete_ds_asr")
+plot_stt(
+  daisie_data_list = amphibian_daisie_data_list_complete_ds_asr,
+  log_num_species = TRUE,
+  median_res = 0.1
+)
+
+data("amphibian_daisie_data_list_dna_ds_asr")
+plot_stt(
+  daisie_data_list = amphibian_daisie_data_list_dna_ds_asr,
+  log_num_species = TRUE,
+  median_res = 0.1
+)
---
+++ inst/scripts/plots/mammals/plot_mammal_stt.R
@@ -0,0 +1,13 @@
+data("mammal_daisie_data_list_complete_ds_asr")
+plot_stt(
+  daisie_data_list = mammal_daisie_data_list_complete_ds_asr,
+  log_num_species = TRUE,
+  median_res = 0.1
+)
+
+data("mammal_daisie_data_list_dna_ds_asr.rda")
+plot_stt(
+  daisie_data_list = mammal_daisie_data_list_dna_ds_asr,
+  log_num_species = TRUE,
+  median_res = 0.1
+)
