410cf1536e04c045c8b49b9b830a8bc4685298da
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-07-10 22:34:13 +0200

    added plot_col_time script and col time plots

---
+++ inst/scripts/plots/plot_col_time.R
@@ -0,0 +1,71 @@
+amphibians <- readRDS(file = system.file(
+  "extdata",
+  "extracted_data",
+  "amphibian_data",
+  "amphibian_island_tbl_complete_ds_asr.rds",
+  package = "MadIsland",
+  mustWork = TRUE
+))
+
+mammals <- readRDS(file = system.file(
+  "extdata",
+  "extracted_data",
+  "mammal_data",
+  "mammal_island_tbl_complete_ds_asr.rds",
+  package = "MadIsland",
+  mustWork = TRUE
+))
+
+squamates <- readRDS(file = system.file(
+  "extdata",
+  "extracted_data",
+  "squamate_data",
+  "squamate_island_tbl_complete_ds_asr.rds",
+  package = "MadIsland",
+  mustWork = TRUE
+))
+
+multi_island_tbl_list <- list(
+  Amphibians = amphibians,
+  Mammals = mammals,
+  Squamates = squamates
+)
+
+
+stem_col_time <- plot_col_island_tbl(
+  multi_island_tbl_list = multi_island_tbl_list,
+  col_time = "stem"
+)
+
+ggplot2::ggsave(
+  plot = stem_col_time,
+  filename = file.path(
+    "inst",
+    "plots",
+    "stem_col_time.png"
+  ),
+  device = "png",
+  width = 180,
+  height = 180,
+  units = "mm",
+  dpi = 600
+)
+
+crown_col_time <- plot_col_island_tbl(
+  multi_island_tbl_list = multi_island_tbl_list,
+  col_time = "crown"
+)
+
+ggplot2::ggsave(
+  plot = stem_col_time,
+  filename = file.path(
+    "inst",
+    "plots",
+    "crown_col_time.png"
+  ),
+  device = "png",
+  width = 180,
+  height = 180,
+  units = "mm",
+  dpi = 600
+)
