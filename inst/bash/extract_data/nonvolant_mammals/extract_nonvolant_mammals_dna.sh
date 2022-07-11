999ce99bb46194079281d3644281c29dbf590117
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-07-11 19:06:00 +0200

    split extract mammal bash scripts into volant and nonvolant mammals

---
+++ inst/bash/extract_data/nonvolant_mammals/extract_nonvolant_mammals_dna.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --time=70:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=extract_nonvolant_mammals_dna
+#SBATCH --output=logs/extract_nonvolant_mammals_dna.log
+#SBATCH --mem=5GB
+#SBATCH --partition=gelifes
+
+ml R
+Rscript inst/scripts/extract_data/nonvolant_mammals/extract_nonvolant_mammals_dna.R
---
+++ inst/bash/extract_data/nonvolant_mammals/extract_nonvolant_mammals_dna_ds.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --time=70:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=extract_nonvolant_mammals_dna_ds
+#SBATCH --output=logs/extract_nonvolant_mammals_dna_ds.log
+#SBATCH --mem=5GB
+#SBATCH --partition=gelifes
+
+ml R
+Rscript inst/scripts/extract_data/nonvolant_mammals/extract_nonvolant_mammals_dna_ds.R
