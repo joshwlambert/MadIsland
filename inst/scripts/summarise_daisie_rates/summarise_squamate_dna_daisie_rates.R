a3b9cc9cdf977c2212efc59160868e103d6513f8
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-08-06 15:36:38 +0200

    updated summarise daisie rates dna scripts

--- inst/scripts/summarise_daisie_rates/summarise_squamate_dna_daisie_rates.R
+++ inst/scripts/summarise_daisie_rates/summarise_squamate_dna_daisie_rates.R
@@ -1,19 +1,5 @@
 #summarise squamate daisie rates dna data
 
-squamate_oceanic_rates <- readRDS(
-  file = system.file(
-    "extdata",
-    "collated_daisie_output",
-    "squamate_daisie_output",
-    "squamate_dna_oceanic.rds",
-    package = "MadIsland",
-    mustWork = TRUE
-  )
-)
-
-summarise_daisie_rates(data = squamate_oceanic_rates)
-
-
 squamate_nonoceanic_rates <- readRDS(
   file = system.file(
     "extdata",
--- inst/scripts/summarise_daisie_rates/summarise_volant_mammal_dna_daisie_rates.R
+++ inst/scripts/summarise_daisie_rates/summarise_volant_mammal_dna_daisie_rates.R
@@ -1,19 +1,5 @@
 #summarise volant mammal daisie rates dna data
 
-volant_mammal_oceanic_rates <- readRDS(
-  file = system.file(
-    "extdata",
-    "collated_daisie_output",
-    "volant_mammal_daisie_output",
-    "volant_mammal_dna_oceanic.rds",
-    package = "MadIsland",
-    mustWork = TRUE
-  )
-)
-
-summarise_daisie_rates(data = volant_mammal_oceanic_rates)
-
-
 volant_mammal_nonoceanic_rates <- readRDS(
   file = system.file(
     "extdata",
