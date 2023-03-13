c2daf7c9ad2295423f92a4894d92c0ea545fdc16
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-03-13 20:12:02 +0000

    updated count_missing_species tests to pass

--- tests/testthat/test-count_missing_species.R
+++ tests/testthat/test-count_missing_species.R
@@ -1,8 +1,17 @@
-test_that("count_missing_species works for mammals", {
-  mammal_checklist <- read_checklist(file_name = "mammal_checklist.csv")
-  missing_mammals <- count_missing_species(checklist = mammal_checklist)
+test_that("count_missing_species works for non-volant mammals", {
+  mammal_checklist <- read_checklist(
+    file_name = "nonvolant_mammal_checklist.csv"
+  )
+  missing_mammals <- count_missing_species(
+    checklist = mammal_checklist,
+    dna_or_complete = "dna",
+    daisie_status = TRUE
+  )
   expect_true(is.data.frame(missing_mammals))
-  expect_equal(ncol(missing_mammals), 2)
-  expect_equal(nrow(missing_mammals), 21)
-  expect_equal(colnames(missing_mammals), c("clade_name", "missing_species"))
+  expect_equal(ncol(missing_mammals), 3)
+  expect_equal(nrow(missing_mammals), 28)
+  expect_equal(
+    colnames(missing_mammals),
+    c("clade_name", "missing_species", "endemicity_status")
+  )
 })
