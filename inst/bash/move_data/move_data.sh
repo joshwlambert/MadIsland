5b8c8e16d854a8ac3f58e929143f1c7ee2eb6bff
joshwlambert <joshua.lambert@lshtm.ac.uk>
2023-11-23 23:15:20 +0000

    add bash script to move data

---
+++ inst/bash/move_data/move_data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --time=70:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=move_data
+#SBATCH --output=logs/move_data.log
+#SBATCH --mem=5GB
+#SBATCH --partition=gelifes
+
+ml R
+Rscript data-raw/amphibian_daisie_data_list_complete_ds_asr.R
+Rscript data-raw/amphibian_daisie_data_list_dna_ds_asr.R
+Rscript data-raw/bird_daisie_data_list_complete_ds_asr.R
+Rscript data-raw/bird_daisie_data_list_dna_ds_asr.R
+Rscript data-raw/nonvolant_mammal_daisie_data_list_complete_ds_asr.R
+Rscript data-raw/nonvolant_mammal_daisie_data_list_dna_ds_asr.R
+Rscript data-raw/squamate_daisie_data_list_complete_ds_asr.R
+Rscript data-raw/squamate_daisie_data_list_dna_ds_asr.R
+Rscript data-raw/volant_mammal_daisie_data_list_complete_ds_asr.R
+Rscript data-raw/volant_mammal_daisie_data_list_dna_ds_asr.R
+echo "Finished saving extracted data in data dir"
