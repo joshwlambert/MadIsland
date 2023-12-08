9ae67399d15bf10847e5ae0736841a4a80cefcfe
joshwlambert <joshua.lambert@lshtm.ac.uk>
2023-12-08 16:43:42 +0000

    updated move_data bash script

--- inst/bash/move_data/move_data.sh
+++ inst/bash/move_data/move_data.sh
@@ -8,14 +8,14 @@
 #SBATCH --partition=gelifes
 
 ml R
-Rscript data-raw/amphibian_daisie_data_list_complete_ds_asr.R
-Rscript data-raw/amphibian_daisie_data_list_dna_ds_asr.R
-Rscript data-raw/bird_daisie_data_list_complete_ds_asr.R
-Rscript data-raw/bird_daisie_data_list_dna_ds_asr.R
-Rscript data-raw/nonvolant_mammal_daisie_data_list_complete_ds_asr.R
-Rscript data-raw/nonvolant_mammal_daisie_data_list_dna_ds_asr.R
-Rscript data-raw/squamate_daisie_data_list_complete_ds_asr.R
-Rscript data-raw/squamate_daisie_data_list_dna_ds_asr.R
-Rscript data-raw/volant_mammal_daisie_data_list_complete_ds_asr.R
-Rscript data-raw/volant_mammal_daisie_data_list_dna_ds_asr.R
+Rscript data-raw/amp_ddl_complete_ds_asr.R
+Rscript data-raw/amp_ddl_dna_ds_asr.R
+Rscript data-raw/bird_ddl_complete_ds_asr.R
+Rscript data-raw/bird_ddl_dna_ds_asr.R
+Rscript data-raw/nvm_ddl_complete_ds_asr.R
+Rscript data-raw/nvm_ddl_dna_ds_asr.R
+Rscript data-raw/squa_ddl_complete_ds_asr.R
+Rscript data-raw/squa_ddl_dna_ds_asr.R
+Rscript data-raw/vm_ddl_complete_ds_asr.R
+Rscript data-raw/vm_ddl_dna_ds_asr.R
 echo "Finished saving extracted data in data dir"
