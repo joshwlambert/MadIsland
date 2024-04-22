0f4f28ce87840f2a3de463e897c51754f16ae231
joshwlambert <joshua.lambert@lshtm.ac.uk>
2024-04-22 21:08:59 +0100

    updated checklist_stats script to give data.frame table

--- inst/scripts/checklist_stats/checklist_stats.R
+++ inst/scripts/checklist_stats/checklist_stats.R
@@ -2,28 +2,36 @@
 
 amphibians <- read_checklist(file_name = "amp_checklist.csv")
 head(amphibians)
-checklist_stats(amphibians)
+amp <- checklist_stats(amphibians)
 
 # Birds
 
 birds <- read_checklist(file_name = "bird_checklist.csv")
 head(birds)
-checklist_stats(birds)
+birds <- checklist_stats(birds)
 
 # Non-volant Mammals
 
 nonvolant_mammals <- read_checklist(file_name = "nvm_checklist.csv")
 head(nonvolant_mammals)
-checklist_stats(nonvolant_mammals)
+nvm <- checklist_stats(nonvolant_mammals)
 
 # Volant Mammals
 
 volant_mammals <- read_checklist(file_name = "vm_checklist.csv")
 head(volant_mammals)
-checklist_stats(volant_mammals)
+vm <- checklist_stats(volant_mammals)
 
 # Squamates
 
 squamates <- read_checklist(file_name = "squa_checklist.csv")
 head(squamates)
-checklist_stats(squamates)
+squamates <- checklist_stats(squamates)
+
+data.frame(
+  amp = unlist(amp),
+  bird = unlist(bird),
+  nvm = unlist(nvm),
+  squa = unlist(squa),
+  vm = unlist(vm)
+)
