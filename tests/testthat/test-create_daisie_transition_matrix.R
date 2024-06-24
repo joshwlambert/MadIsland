69d427451f356e154039f1cf8e4e7ec12bd057ff
joshwlambert <joshua.lambert@lshtm.ac.uk>
2024-06-24 07:21:58 +0100

    updated create_daisie_transition_matrix doc and tests

--- tests/testthat/test-create_daisie_transition_matrix.R
+++ tests/testthat/test-create_daisie_transition_matrix.R
@@ -5,7 +5,7 @@ test_that("create_daisie_transition_matrix works", {
       data = c(
         0, 1, 0,
         2, 0, 3,
-        2, 4, 0
+        0, 0, 0
       ),
       nrow = 3,
       byrow = TRUE
@@ -13,6 +13,32 @@ test_that("create_daisie_transition_matrix works", {
   )
 })
 
-test_that("create_daisie_transition_matrix warns", {
-  expect_warning(create_daisie_transition_matrix("something"))
+test_that("create_daisie_transition_matrix works with zero endemic ext", {
+  expect_identical(
+    create_daisie_transition_matrix(zero_endemic_ext = FALSE),
+    matrix(
+      data = c(
+        0, 1, 0,
+        2, 0, 3,
+        4, 0, 0
+      ),
+      nrow = 3,
+      byrow = TRUE
+    )
+  )
+})
+
+test_that("create_daisie_transition_matrix works with zero endemic back col", {
+  expect_identical(
+    create_daisie_transition_matrix(zero_endemic_back_col = FALSE),
+    matrix(
+      data = c(
+        0, 1, 0,
+        2, 0, 3,
+        0, 4, 0
+      ),
+      nrow = 3,
+      byrow = TRUE
+    )
+  )
 })
