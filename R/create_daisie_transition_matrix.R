fb06f2b8391e4c85aaa533c1e609c2e0b1187f00
joshwlambert <joshua.lambert@lshtm.ac.uk>
2024-06-24 07:19:02 +0100

    add zero_endemic_ext and zero_endemic_back_col args to create_daisie_transition_matrix

--- R/create_daisie_transition_matrix.R
+++ R/create_daisie_transition_matrix.R
@@ -13,24 +13,34 @@
 #' to state 1), back-colonisation from the island is not allowed (state 3 to
 #' state 2), and anagenesis has its own rate (state 2 to state 3).
 #'
-#' @param ... [dots] Not used, warns if arguments are supplied.
+#' @param zero_endemic_ext Set the extinction rate of endemic species to zero.
+#' Default is `TRUE`, if `FALSE` then the endemic species (state 3) can go to
+#' state 1 (not present on the island).
+#' @param zero_endemic_back_col Set the back colonisations rate of endemic
+#' species to zero. Default is `TRUE`, if `FALSE` then the endemic species
+#' (state 3) can go to state 2 (non-endemic on the island).
 #'
 #' @return A 3x3 integer matrix.
 #' @export
 #'
 #' @examples
 #' create_daisie_transition_matrix
-create_daisie_transition_matrix <- function(...) {
-  chkDots(...)
-  return(
-    matrix(
-      data = c(
-        0, 1, 0,
-        2, 0, 3,
-        0, 0, 0
-      ),
-      nrow = 3,
-      byrow = TRUE
-    )
+create_daisie_transition_matrix <- function(zero_endemic_ext = TRUE,
+                                            zero_endemic_back_col = TRUE) {
+  daisie_matrix <- matrix(
+    data = c(
+      0, 1, 0,
+      2, 0, 3,
+      0, 0, 0
+    ),
+    nrow = 3,
+    byrow = TRUE
   )
+  if (isFALSE(zero_endemic_ext)) {
+    daisie_matrix[3, 1] <- max(daisie_matrix) + 1
+  }
+  if (isFALSE(zero_endemic_back_col)) {
+    daisie_matrix[3, 2] <- max(daisie_matrix) + 1
+  }
+  return(daisie_matrix)
 }
